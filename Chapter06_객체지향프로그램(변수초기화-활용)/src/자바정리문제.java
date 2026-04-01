ab89ac8d4931ce134ddb8373369d3fa65d20a274
sist <sist@DESKTOP-AAB715J>
2026-04-02 08:57:03 +0900

    2026-03-18

---
+++ Chapter06_객체지향프로그램(변수초기화-활용)/src/자바정리문제.java
@@ -0,0 +1,95 @@
+
+public class 자바정리문제 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		class movie
+		{
+			
+			String poster;
+			String title;
+			String regdate;
+			String rating;
+			String genre;
+			String rank;
+			String country;
+			String grade;
+			String length;
+			String content;
+		}
+		
+		class news
+		{
+			String title;
+			String photo;
+			String cotent;
+			String date;
+			String source;           
+		}
+		
+		class foodbest
+		{
+			String photo;
+			String title;
+			String explanation;
+			
+		}
+		
+		class restaurant
+		{
+			String title;
+			String rating;
+			String address;
+			String phone;
+			String type;
+			String price;
+			String parking;
+			String time;
+			String menu;
+			
+		}
+		
+		class recipe
+		{
+			String video;
+			String title;
+			String source;
+			String rating;
+			String views;
+		}
+		
+		class music
+		{
+			int rank;
+			String poster;
+			String title;
+			String singer;
+			String interest;
+			
+		}
+		
+		class market
+		{
+			String sale;
+			String photo;
+			String title;
+			String price;
+		}
+		
+		class theater
+		{
+			String poster;
+			String title;
+			boolean status;
+			String genre;
+			String date;
+			String spot;
+			String actor;
+			String grade;
+			String time;
+		}
+		
+	}
+
+}

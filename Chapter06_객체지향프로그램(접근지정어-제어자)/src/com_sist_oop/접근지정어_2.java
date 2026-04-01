3b6346fc3f9e682664a7d6cf88129b384e3877b8
sist <sist@DESKTOP-AAB715J>
2026-04-02 08:58:07 +0900

    2026-03-20

---
+++ Chapter06_객체지향프로그램(접근지정어-제어자)/src/com_sist_oop/접근지정어_2.java
@@ -0,0 +1,13 @@
+package com_sist_oop;
+
+import com_sist_oop2.Music;
+
+public class 접근지정어_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Music m=new Music();
+		
+	}
+
+}
---
+++ Chapter06_객체지향프로그램(접근지정어-제어자)/src/com_sist_oop2/Music.java
@@ -0,0 +1,15 @@
+package com_sist_oop2;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Music {
+	private int mno;
+	private String title;
+	private String singer;
+	protected String album;
+	int hit;
+
+}
